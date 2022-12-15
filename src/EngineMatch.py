0082e398ac7b4614a7e6eb805f3c8cc15177f31a
Quintium <68058512+Quintium@users.noreply.github.com>
2022-12-15 20:50:18 +0100

    Commenting/formatting, solves #9 and #30 (#31)

--- src/EngineMatch.py
+++ src/EngineMatch.py
@@ -1,88 +1,116 @@
 import time, signal, traceback
-import multiprocessing
 import chess, chess.engine
 from multiprocessing import Pool, Manager
 from tqdm import tqdm
 from inspect import FrameInfo
 from Results import Results, SharedResults, MatchEvent, ErrorEvent
 
+# Play match between two engines
 def engineMatch(engines: list, games: int, timeLimit: float, processes: int, totalGames: int) -> Results:
+    # Handle keyboard interrupt in main process
     def handleKeyboardInterrupt(sig: int, frame: FrameInfo):
         sharedResults.stopMatch()
 
+    # Create shared results
     manager = Manager()
     sharedResults = SharedResults(engines[0], engines[1], 0, 0, 0, timeLimit, manager)
 
+    # Create match output (engine names and progress bar)
     print(f"Engine match: {engines[0].fullName()} vs {engines[1].fullName()}")
     progressBar = tqdm(desc=f"Score: {sharedResults.scoreString()}", total=totalGames, dynamic_ncols=True, unit="games")
 
+    # Create pool of processes
     with Pool(processes) as pool:
+        # Distribute games across processes with same amount of white and black pieces
         inputs = [(i % 2, sharedResults) for i in range(games)]
         asyncResult = pool.starmap_async(engineGame, inputs)
         
+        # Handle keyboard interrupts
         signal.signal(signal.SIGINT, handleKeyboardInterrupt)
+
+        # Wait until games are ready
         while not asyncResult.ready():
             time.sleep(0.1)
+
+            # Handle all events sent by child processes
             while sharedResults.hasEvent():
                 event = sharedResults.getEvent()
                 if isinstance(event, MatchEvent):
+                    # Update score after completed match
                     progressBar.set_description(f"Score: {sharedResults.scoreString()}")
                     progressBar.update(1)
                 elif isinstance(event, ErrorEvent):
+                    # Output error after one is encountered
                     progressBar.write(event.error)
 
-    asyncResult.wait() 
+    # Cleanup
     progressBar.close()
     print()
 
+    # Return pure results
     return sharedResults.toResults()
 
-def engineGame(whitePlayer: int, results: SharedResults) -> None:
+# Play an engine game in a child process
+def engineGame(whiteEngine: int, results: SharedResults) -> None:
+    # Ignore keyboard interrupts as they are handled by the main process
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
     try:
-        engineProcesses = [results.player1.createProcess(), results.player2.createProcess()]
+        # Create processes from engines
+        engineProcesses = [results.engine1.createProcess(), results.engine2.createProcess()]
+
+        # Create new chess board
         board = chess.Board()
         game = []
         
+        # Play chess moves until the game ends or child process is aborted
         while (board.outcome(claim_draw=True) == None) and not results.wasStopped():
-            engineNr = whitePlayer if board.turn == chess.WHITE else not whitePlayer
+            # Check which engine's move it is
+            engineNr = whiteEngine if board.turn == chess.WHITE else not whiteEngine
             
             try:
+                # Get the engine's move choice
                 result = engineProcesses[engineNr].play(board, chess.engine.Limit(time=results.timeLimit))
             except chess.engine.EngineError as e:
-                engineName = results.player1.fullName() if engineNr == 0 else results.player2.fullName()
+                # Handle engine errors by passing on a string of played moves to the error
+                engineName = results.engine1.fullName() if engineNr == 0 else results.engine2.fullName()
                 gameString = " ".join(game)
                 raise chess.engine.EngineError(f"Engine error occured in engine '{engineName}' after moves: playing the moves '{gameString}'") from e
                 
+            # Play move on the board
             board.push(result.move)
             game.append(result.move.uci())
 
+        # Close engines upon abortion
         if results.wasStopped():
             for engineProcess in engineProcesses:
                 engineProcess.close()
             return
 
+        # Check which color won
         winnerColor = board.outcome(claim_draw=True).winner
 
-        with results.lock:
-            if winnerColor == None:
-                results.addDraws(1)
+        # Change results based on who won (or draw)
+        if winnerColor == None:
+            results.addDraws(1)
+        else:
+            if winnerColor == chess.WHITE:
+                winnerEngine = whiteEngine
             else:
-                if winnerColor == chess.WHITE:
-                    winnerPlayer = whitePlayer
-                else:
-                    winnerPlayer = not whitePlayer
+                winnerEngine = not whiteEngine
 
-                if winnerPlayer == 0:
-                    results.addPlayer1Wins(1)
-                else:
-                    results.addPlayer2Wins(1)
+            if winnerEngine == 0:
+                results.addEngine1Wins(1)
+            else:
+                results.addEngine2Wins(1)
 
+        # Pass on that the event was completed
         results.putEvent(MatchEvent())
 
+    # Send all errors to main process
     except Exception:
         results.putEvent(ErrorEvent(traceback.format_exc()))
 
+    # Close engines (important)
     for engineProcess in engineProcesses:
         engineProcess.close()
--- src/Results.py
+++ src/Results.py
@@ -2,54 +2,60 @@ import math
 from multiprocessing import Manager, Value, Lock, Queue, Event
 from Engine import Engine
 
+# Class that contains results of an engine match
 class Results:
-    player1: Engine
-    player2: Engine
-    player1Wins: int
-    player2Wins: int
+    engine1: Engine
+    engine2: Engine
+    engine1Wins: int
+    engine2Wins: int
     draws: int
     timeLimit: float
 
-    def __init__(self, player1: Engine, player2: Engine, player1Wins: int, player2Wins: int, draws: int, timeLimit: float):
-        self.player1 = player1
-        self.player2 = player2
-        self.player1Wins = player1Wins
-        self.player2Wins = player2Wins
+    # Create results based ong given stats
+    def __init__(self, engine1: Engine, engine2: Engine, engine1Wins: int, engine2Wins: int, draws: int, timeLimit: float):
+        self.engine1 = engine1
+        self.engine2 = engine2
+        self.engine1Wins = engine1Wins
+        self.engine2Wins = engine2Wins
         self.draws = draws
         self.timeLimit = timeLimit
 
-    def getPlayer1Wins(self) -> int:
-        return self.player1Wins
+    # Get match stats
+    def getEngine1Wins(self) -> int:
+        return self.engine1Wins
 
-    def getPlayer2Wins(self) -> int:
-        return self.player2Wins
+    def getEngine2Wins(self) -> int:
+        return self.engine2Wins
 
     def getDraws(self) -> int:
         return self.draws
 
-    def addPlayer1Wins(self, n) -> None:
-        self.player1Wins += n
+    # Change match stats
+    def addEngine1Wins(self, n) -> None:
+        self.engine1Wins += n
 
-    def addPlayer2Wins(self, n) -> None:
-        self.player2Wins += n
+    def addEngine2Wins(self, n) -> None:
+        self.engine2Wins += n
 
     def addDraws(self, n) -> None:
         self.draws += n
 
+    # More match stats not stored in variables
     def gameAmount(self) -> int:
-        return self.getPlayer1Wins() + self.getPlayer2Wins() + self.getDraws()
+        return self.getEngine1Wins() + self.getEngine2Wins() + self.getDraws()
 
-    def player1Score(self) -> int:
-        return self.getPlayer1Wins() + self.getDraws() / 2
+    def engine1Score(self) -> int:
+        return self.getEngine1Wins() + self.getDraws() / 2
 
-    def player2Score(self) -> int:
-        return self.getPlayer2Wins() + self.getDraws() / 2
+    def engine2Score(self) -> int:
+        return self.getEngine2Wins() + self.getDraws() / 2
 
+    # Calculate elo difference of engines after match
     def eloDifference(self) -> float:
         if self.gameAmount() == 0:
             return float("nan")
 
-        expectedScore = self.player2Score() / self.gameAmount()
+        expectedScore = self.engine2Score() / self.gameAmount()
         if expectedScore == 0:
             return 10000
         elif expectedScore == 1:
@@ -57,23 +63,25 @@ class Results:
         else:
             return round(400 * math.log10(1 / expectedScore - 1), 2)
 
+    # Convert elo difference to string
     def eloDifferenceString(self) -> str:   
         eloDiff = self.eloDifference()
         return ("+" if eloDiff > 0 else "") + str(eloDiff)
 
-    def los(self) -> float: # Likelihood of superiority
-        if self.getPlayer1Wins() + self.getPlayer2Wins() == 0:
-            return float("nan")
-        if self.player1Wins == self.player2Wins:
-            return 50
-        unroundedLos = 0.5 * (1 + math.erf((self.getPlayer1Wins() - self.getPlayer2Wins()) / math.sqrt(2 * (self.getPlayer1Wins() + self.getPlayer2Wins()))))
+    # Calculate likelihood of superiority: likelihood that engine 1 is superior to engine 2
+    def los(self) -> float:
+        if self.getEngine1Wins() + self.getEngine2Wins() == 0:
+            return float("nan") # No LOS calculatable
+        unroundedLos = 0.5 * (1 + math.erf((self.getEngine1Wins() - self.getEngine2Wins()) / math.sqrt(2 * (self.getEngine1Wins() + self.getEngine2Wins()))))
         return round(unroundedLos * 100, 2)
 
+    # Create string out of score
     def scoreString(self):
-        return f"{self.getPlayer1Wins()} - {self.getPlayer2Wins()} - {self.getDraws()}"
+        return f"{self.getEngine1Wins()} - {self.getEngine2Wins()} - {self.getDraws()}"
 
+    # Convert stats to multi-line string
     def statString(self):
-        message = (f"Engine match: {self.player1.fullName()} vs {self.player2.fullName()}:\n"
+        message = (f"Engine match: {self.engine1.fullName()} vs {self.engine2.fullName()}:\n"
                    f"Time Limit: {self.timeLimit}\n"
                    f"Games played: {self.gameAmount()}\n"
                    f"Final score: {self.scoreString()}\n"
@@ -82,52 +90,61 @@ class Results:
 
         return message
 
+# Class for sharing results between processes using multiprocessing
 class SharedResults(Results):
-    player1: Engine
-    player2: Engine
-    player1Wins: Value
-    player2Wins: Value
+    engine1: Engine
+    engine2: Engine
+    engine1Wins: Value
+    engine2Wins: Value
     draws: Value
     timeLimit: float
-    stop: Event
-    lock: Lock
-    eventQueue: Queue
+    stop: Event # Event for stopping match prematurely
+    lock: Lock # Lock for incrementing wins/draws
+    eventQueue: Queue # Queue for events from child processes to main process
 
-    def __init__(self, player1: Engine, player2: Engine, player1Wins: int, player2Wins: int, draws: int, timeLimit: float, manager: Manager):
-        self.player1 = player1
-        self.player2 = player2
-        self.player1Wins = manager.Value("i", player1Wins)
-        self.player2Wins = manager.Value("i", player2Wins)
+    # Create shared results using stats and multiprocessing manager
+    def __init__(self, engine1: Engine, engine2: Engine, engine1Wins: int, engine2Wins: int, draws: int, timeLimit: float, manager: Manager):
+        self.engine1 = engine1
+        self.engine2 = engine2
+        self.engine1Wins = manager.Value("i", engine1Wins)
+        self.engine2Wins = manager.Value("i", engine2Wins)
         self.draws = manager.Value("i", draws)
         self.timeLimit = timeLimit
         self.stop = manager.Event()
         self.lock = manager.Lock()
         self.eventQueue = manager.Queue()
 
-    def getPlayer1Wins(self) -> int:
-        return self.player1Wins.value
+    # Get match stats
+    def getEngine1Wins(self) -> int:
+        return self.engine1Wins.value
 
-    def getPlayer2Wins(self) -> int:
-        return self.player2Wins.value
+    def getEngine2Wins(self) -> int:
+        return self.engine2Wins.value
 
     def getDraws(self) -> int:
         return self.draws.value
 
-    def addPlayer1Wins(self, n) -> None:
-        self.player1Wins.value += n
+    # Change match stats (with lock since increments of multiprocessing.Value are not atomic)
+    def addEngine1Wins(self, n) -> None:
+        with self.lock:
+            self.engine1Wins.value += n
 
-    def addPlayer2Wins(self, n) -> None:
-        self.player2Wins.value += n
+    def addEngine2Wins(self, n) -> None:
+        with self.lock:
+            self.engine2Wins.value += n
 
     def addDraws(self, n) -> None:
-        self.draws.value += n
+        with self.lock:
+            self.draws.value += n
 
+    # Stopping matches
     def stopMatch(self) -> None:
         self.stop.set()
 
     def wasStopped(self) -> bool:
         return self.stop.is_set()
 
+    # Managing events
     def putEvent(self, event: Event) -> None:
         self.eventQueue.put(event)
 
@@ -137,15 +154,19 @@ class SharedResults(Results):
     def getEvent(self) -> Event:
         return self.eventQueue.get()
 
+    # Converting to pure results object
     def toResults(self) -> Results:
-        return Results(self.player1, self.player2, self.getPlayer1Wins(), self.getPlayer2Wins(), self.getDraws(), self.timeLimit)
+        return Results(self.engine1, self.engine2, self.getEngine1Wins(), self.getEngine2Wins(), self.getDraws(), self.timeLimit)
 
+# General class for events
 class Event:
     pass
 
+# Event for completed match
 class MatchEvent(Event):
     pass
 
+# Event for error encountered
 class ErrorEvent(Event):
     error: str
 
