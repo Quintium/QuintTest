422d147d5ce43b17beba84b2d02614e5b6ea0e9b
Quintium <yzmailer@gmail.com>
2022-12-15 21:48:37 +0100

    Better name and path management

--- src/Engine.py
+++ src/Engine.py
@@ -1,3 +1,4 @@
+import os, shutil
 import chess, chess.engine
 
 # Class for managing engines and their executables
@@ -12,8 +13,30 @@ class Engine:
 
     # Give full name of engine, potentially stripping of .exe and adding command line arguments
     def fullName(self) -> str:
-        return (self.name[:-4] if self.name.endswith(".exe") else self.name) + ("_" if self.args else "") + "_".join([str(arg) for arg in self.args])
+        name = self.name
+
+        # Omit file extension
+        if "." in name:
+            name = name[: name.find(".")]
+        
+        # Add command line arguments to name
+        if self.args:
+            name += "_".join([str(arg) for arg in self.args])
+
+        return name
+
+    # Get path of engine executable
+    def path(self) -> None:
+        # Check if file exists in main directory
+        if shutil.which(self.name, os.X_OK):
+            return self.name
+
+        # Check if file exists in engine directory
+        if shutil.which("engines/" + self.name, os.X_OK):
+            return "engines/" + self.name
+        
+        raise RuntimeError(f"No executable found at [engines\]{self.name}")
 
     # Create engine process for engine
     def createProcess(self) -> chess.engine.SimpleEngine:
-        return chess.engine.SimpleEngine.popen_uci(["engines/" + self.name] + [str(arg) for arg in self.args], setpgrp=True) # New process group, so keyboard interrupts aren't passed on
+        return chess.engine.SimpleEngine.popen_uci([self.path()] + [str(arg) for arg in self.args], setpgrp=True) # New process group, so keyboard interrupts aren't passed on
