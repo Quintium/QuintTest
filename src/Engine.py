c3f307aa8e24e0190d653df9a8cccae0443d3e82
Quintium <yzmailer@gmail.com>
2022-12-18 21:45:35 +0100

    Popen file checking, error raising

--- src/Engine.py
+++ src/Engine.py
@@ -1,15 +1,21 @@
-import os, shutil, glob, subprocess
+import os, shutil, glob, subprocess, errno
 import chess, chess.engine
 
 # Class for managing engines and their executables
 class Engine:
     name: str
     args: list
+    path: str
 
     # Create engine based on name and command line arguments
     def __init__(self, name: str, args: list):
         self.name = name
         self.args = args
+
+        # Save path of engine, if no path is found return error
+        self.path = self.getPath()
+        if self.path == None:
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), "[engines/]" + self.name)
 
     # Give full name of engine, potentially stripping of .exe and adding command line arguments
     def fullName(self) -> str:
@@ -29,20 +35,18 @@ class Engine:
     def pathExec(self, path: str) -> bool:
         try:
             # Try to run file with path, exit after 1ms, return False if file exits after less than 1ms (engines should wait for output)
-            subprocess.run(path, check=True, timeout=0.001, stdout=subprocess.DEVNULL)
-            return False
-        except FileNotFoundError:
+            p = subprocess.Popen(path, stdout=subprocess.DEVNULL)
+            p.terminate()
+            return True
+        except OSError:
             # Return False if file isn't found
             return False
         except subprocess.CalledProcessError:
-            # Return False if file can't be called
+            # Return False if file returns error
             return False
-        except subprocess.TimeoutExpired:
-            # Return True if file runs more than 1ms
-            return True
 
     # Get path of engine executable
-    def path(self) -> None:
+    def getPath(self) -> None:
         # Check if file exists in main directory
         if self.pathExec(self.name):
             return self.name
@@ -51,8 +55,8 @@ class Engine:
         if self.pathExec("engines/" + self.name):
             return "engines/" + self.name
         
-        raise RuntimeError(f"No executable found at [engines\]{self.name}")
+        return None
 
     # Create engine process for engine
     def createProcess(self) -> chess.engine.SimpleEngine:
-        return chess.engine.SimpleEngine.popen_uci([self.path()] + [str(arg) for arg in self.args], setpgrp=True) # New process group, so keyboard interrupts aren't passed on
+        return chess.engine.SimpleEngine.popen_uci([self.path] + [str(arg) for arg in self.args], setpgrp=True) # New process group, so keyboard interrupts aren't passed on
