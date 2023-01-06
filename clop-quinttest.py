cc2e6f855a4f3bb23eb63222a3f0393889be4fd2
Quintium <yzmailer@gmail.com>
2023-01-06 16:28:43 +0100

    Comment CLOP script

--- clop-quinttest.py
+++ clop-quinttest.py
@@ -1,26 +1,30 @@
-import sys, io, yaml
+import sys, yaml
 from yaml.loader import SafeLoader
 import src.EngineMatch
 from src.Engine import Engine
 from src.TimeControl import TimeControl
 
 def main():
+    # Check if there are sufficient arguments
     if len(sys.argv) < 5:
         sys.stderr.write("Too few arguments\n")
         return 2
 
+    # Parse seed
     try:
         seed = int(sys.argv[2])
     except:
         sys.stderr.write(f"Invalid seed value: {sys.argv[2]}\n")
         return 2
 
+    # Parse engine parameters
     try:
         engineParams = [float(param) for param in sys.argv[4::2]]
     except:
         sys.stderr.write(f"Invalid parameter values\n")
         return 2
 
+    # Parse options for testing from yaml file
     try:
         with open("clop-quinttest-options.yaml", "r") as file:
             data = yaml.load(file, Loader=SafeLoader)
@@ -32,20 +36,23 @@ def main():
         sys.stderr.write(f"Insufficient/invalid options in clop-quinttest-options.yaml\n")
         return 2
 
+    # Create engine objects, determine who's white from the seed
     engine1 = Engine(engineName, engineParams)
     engine2 = Engine(opponentName, opponentParams)
-
     if seed % 2 == 1:
         engine1, engine2 = engine2, engine1
 
+    # Create time control object
     timeControl = TimeControl(timeControlString)
 
+    # Complete engine match
     try:
         results = src.EngineMatch.engineMatch([engine1, engine2], 1, timeControl, 1, True)
     except:
         sys.stderr.write("Engine match failed\n")
         return 1
 
+    # Check which engine won
     if results.getEngine1Wins() == 1:
         result = 1
     elif results.getEngine2Wins() == 1:
@@ -53,9 +60,11 @@ def main():
     else:
         result = 0.5
 
+    # Adjust results for white/black
     if seed % 2 == 1:
         result = 1 - result
 
+    # Print out results
     if result == 1:
         sys.stdout.write('W\n')
     elif result == 0:
